1b7b524cdcaa2e9189558a0482466474e3bba0ed
Eric Saxby <esaxby@pivotal.io>
2016-03-17 11:29:18 -0700

    Update utils.sh to pass bash linter [#102210084]
    Signed-off-by: Zach Robinson <zrobinson@pivotal.io>

--- src/capi_utils/utils.sh
+++ src/capi_utils/utils.sh
@@ -3,23 +3,23 @@
 #
 # When utils.sh is loaded, this sends stdout and stderr to /var/vcap/sys/log.
 # To disable this behavior, set SKIP_SYS_LOG_TEE=1
-tee_output_to_sys_log() {
+function tee_output_to_sys_log() {
   if [ "${SKIP_SYS_LOG_TEE}" = 1 ]; then
     echo "Skipping output redirection to /var/vcap/sys/log" >&2
   else
     mkdir -p /var/vcap/sys/log
 
-    exec > >(tee -a >(logger -p user.info -t vcap.$(basename $0).stdout) | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>/var/vcap/sys/log/$(basename $0).log)
-    exec 2> >(tee -a >(logger -p user.error -t vcap.$(basename $0).stderr) | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>/var/vcap/sys/log/$(basename $0).err.log)
+    exec > >(tee -a >(logger -p user.info -t "vcap.$(basename "$0").stdout") | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>"/var/vcap/sys/log/$(basename "$0").log")
+    exec 2> >(tee -a >(logger -p user.error -t "vcap.$(basename "$0").stderr") | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>"/var/vcap/sys/log/$(basename "$0").err.log")
   fi
 }
 
 tee_output_to_sys_log
 
 
-pid_is_running() {
+function pid_is_running() {
   declare pid="$1"
-  ps -p ${pid} >/dev/null 2>&1
+  ps -p "${pid}" >/dev/null 2>&1
 }
 
 # pid_guard
@@ -31,24 +31,25 @@ pid_is_running() {
 # If an old process is running on the pid found in the :pidfile:, exit 1. Otherwise,
 # remove the stale :pidfile: if it exists.
 #
-pid_guard() {
+function pid_guard() {
   declare pidfile="$1" name="$2"
 
-  echo "------------ STARTING `basename $0` at `date` --------------" | tee /dev/stderr
+  echo "------------ STARTING $(basename "$0") at $(date) --------------" | tee /dev/stderr
 
   if [ ! -f "${pidfile}" ]; then
     return 0
   fi
 
-  local pid=$(head -1 "${pidfile}")
+  local pid
+  pid=$(head -1 "${pidfile}")
 
-  if pid_is_running ${pid}; then
+  if pid_is_running "${pid}"; then
     echo "${name} is already running, please stop it first"
     exit 1
   fi
 
   echo "Removing stale pidfile..."
-  rm ${pidfile}
+  rm "${pidfile}"
 }
 
 # wait_pid_death
@@ -62,22 +63,24 @@ pid_guard() {
 # Note that this should be run in a subshell, so that the current
 # shell does not exit.
 #
-wait_pid_death() {
+function wait_pid_death() {
   declare pid="$1" timeout="$2"
-  local countdown=$(( $timeout * 10 ))
+
+  local countdown
+  countdown=$(( timeout * 10 ))
 
   while true; do
     if [ ${countdown} -le 0 ]; then
       return 1
     fi
 
-    if ! pid_is_running ${pid}; then
+    if ! pid_is_running "${pid}"; then
       return 0
     fi
 
-    [ $(( ${countdown} % 10 )) = '0' ] && echo -n .
+    [ $(( countdown % 10 )) = '0' ] && echo -n .
 
-    countdown=$(( ${countdown} - 1 ))
+    countdown=$(( countdown - 1 ))
     sleep 0.1
   done
 }
@@ -96,42 +99,42 @@ wait_pid_death() {
 # Monit default timeout for start/stop is 30s
 # Append 'with timeout {n} seconds' to monit start/stop program configs
 #
-kill_and_wait() {
-  declare pidfile="$1" \
-    timeout="${2:-25}"
+function kill_and_wait() {
+  declare pidfile="$1" timeout="${2:-25}"
 
   if [ ! -f "${pidfile}" ]; then
     echo "Pidfile ${pidfile} doesn't exist"
     exit 0
   fi
 
-  local pid=$(head -1 "${pidfile}")
+  local pid
+  pid=$(head -1 "${pidfile}")
 
   if [ -z "${pid}" ]; then
     echo "Unable to get pid from ${pidfile}"
     exit 1
   fi
 
-  if ! pid_is_running ${pid}; then
+  if ! pid_is_running "${pid}"; then
     echo "Process ${pid} is not running"
-    rm -f $pidfile
+    rm -f "${pidfile}"
     exit 0
   fi
 
   echo "Killing ${pidfile}: ${pid} "
-  kill ${pid}
+  kill "${pid}"
 
-  if ! $(wait_pid_death ${pid} ${timeout}); then
+  if ! wait_pid_death "${pid}" "${timeout}"; then
     echo -ne "\nKill timed out, using kill -9 on ${pid}... "
-    kill -9 ${pid}
+    kill -9 "${pid}"
     sleep 0.5
   fi
 
-  if pid_is_running ${pid}; then
+  if pid_is_running "${pid}"; then
     echo "Timed Out"
     exit 1
   else
     echo "Stopped"
-    rm -f ${pidfile}
+    rm -f "${pidfile}"
   fi
 }
